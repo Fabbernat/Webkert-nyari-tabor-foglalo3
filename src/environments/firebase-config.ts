fb9f1f54a0a50cd63d1ef4b797995fac84a90e81
Fabian Bernat <fabbernat@gmail.com>
2025-05-02 11:31:24 +0200

    add firebase-config.ts

---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,13 @@
+// environments/firebase-config.ts
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBSLrSsxpMDFq7_xADx57SK4B5zgkCpmoE",
+        authDomain: "webkert-nyari-tabor-foglalo.firebaseapp.com",
+        projectId: "webkert-nyari-tabor-foglalo",
+        storageBucket: "webkert-nyari-tabor-foglalo.firebasestorage.app",
+        messagingSenderId: "595746910150",
+        appId: "1:595746910150:web:e8edc2ad6fd49d4b594cc7",
+        measurementId: "G-2DV7GR8V8F"
+    }
+  };
